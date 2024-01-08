b34769ea0b6e02c44444913a08c5a52ee8cd3c24
unknown <pedro_gonsalves@hotmail.com>
2024-01-08 19:09:27 -0300

    fix: get all articles routes and tag parameter

--- src/main/scala/api/routes/articles/Get.scala
+++ src/main/scala/api/routes/articles/Get.scala
@@ -1,46 +1,45 @@
 package api.routes
 
 import akka.http.scaladsl.model.StatusCodes
-import akka.http.scaladsl.server.Directives
-import api.models.Article
+import akka.http.scaladsl.server.{Directives, Route}
 import api.utils.exceptionHandlers
 import database.Operations
 import org.ektorp.DocumentNotFoundException
 import org.json4s.native.Serialization
-import scala.util.{Success, Failure}
+import scala.util.{Failure, Success}
 
 
 class Get extends Directives {
 
   implicit val formats = org.json4s.DefaultFormats
-  import scala.concurrent.ExecutionContext.Implicits.global
 
   val dbOperations = new Operations
 
-  val route = path("articles") {
-    handleExceptions(exceptionHandlers.articlesExceptionHandler) {
-      get {
-        parameter("tag".as[String].optional) { tag =>
+  val route: Route =
+    path("articles") {
+      handleExceptions(exceptionHandlers.articlesExceptionHandler) {
+        get {
+          parameter("tag".as[String].optional) { tag =>
 
-          if (tag.orNull == null) {
-            val articlesToReturn = dbOperations.getAllDocuments
-            onComplete(articlesToReturn) {
-              case Success(articles) => complete(Serialization.write(articles))
-              case Failure(exception) => complete(StatusCodes.InternalServerError, s"Erro ao obter os artigos: ${exception.getMessage}")
+            if (tag.orNull == null) {
+              val articlesToReturn = dbOperations.getAllDocuments
+              onComplete(articlesToReturn) {
+                case Success(articles) => complete(Serialization.write(articles))
+                case Failure(exception) => complete(StatusCodes.InternalServerError, s"Error when getting the articles. Reason: ${exception.getMessage}")
+              }
             }
-          }
 
-          else {
-            val articlesToReturn = dbOperations.getDocumentsByTag(tag.get)
-            if (articlesToReturn.length > 0) {
-              val jsonArticles = Serialization.write(articlesToReturn)
-              complete(jsonArticles)
-            } else {
-              throw new DocumentNotFoundException(null)
+            else {
+              val articlesToReturn = dbOperations.getDocumentsByTag(tag.get)
+              if (articlesToReturn.length > 0) {
+                val jsonArticles = Serialization.write(articlesToReturn)
+                complete(jsonArticles)
+              } else {
+                throw new DocumentNotFoundException(null)
+              }
             }
           }
         }
       }
     }
-  }
 }
