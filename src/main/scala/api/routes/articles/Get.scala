67b47e7b6371072c8d76ba93553a543dde152072
unknown <pedro_gonsalves@hotmail.com>
2024-01-08 19:08:06 -0300

    add: cors settings

--- src/main/scala/api/routes/articles/Get.scala
+++ src/main/scala/api/routes/articles/Get.scala
@@ -1,31 +1,43 @@
 package api.routes
 
+import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives
+import api.models.Article
 import api.utils.exceptionHandlers
 import database.Operations
 import org.ektorp.DocumentNotFoundException
 import org.json4s.native.Serialization
+import scala.util.{Success, Failure}
 
 
 class Get extends Directives {
 
   implicit val formats = org.json4s.DefaultFormats
+  import scala.concurrent.ExecutionContext.Implicits.global
 
   val dbOperations = new Operations
 
   val route = path("articles") {
     handleExceptions(exceptionHandlers.articlesExceptionHandler) {
       get {
-        parameter("tag".as[String]) { tag =>
-          validate(tag.isEmpty, null) {
-            throw new java.util.NoSuchElementException
+        parameter("tag".as[String].optional) { tag =>
+
+          if (tag.orNull == null) {
+            val articlesToReturn = dbOperations.getAllDocuments
+            onComplete(articlesToReturn) {
+              case Success(articles) => complete(Serialization.write(articles))
+              case Failure(exception) => complete(StatusCodes.InternalServerError, s"Erro ao obter os artigos: ${exception.getMessage}")
+            }
           }
-          val articlesToReturn = dbOperations.getDocumentsByTag(tag)
-          if (articlesToReturn.length > 0) {
-            val jsonArticles = Serialization.write(articlesToReturn)
-            complete(jsonArticles)
-          } else {
-            throw new DocumentNotFoundException(null)
+
+          else {
+            val articlesToReturn = dbOperations.getDocumentsByTag(tag.get)
+            if (articlesToReturn.length > 0) {
+              val jsonArticles = Serialization.write(articlesToReturn)
+              complete(jsonArticles)
+            } else {
+              throw new DocumentNotFoundException(null)
+            }
           }
         }
       }
