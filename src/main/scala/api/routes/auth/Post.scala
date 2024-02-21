482289e0ae31c48ae8f34c5970ab80dba546658c
unknown <pedro_gonsalves@hotmail.com>
2024-02-21 14:55:48 -0300

    refac: improve auth2 with jwt

--- src/main/scala/api/routes/auth/Post.scala
+++ src/main/scala/api/routes/auth/Post.scala
@@ -1,44 +1,64 @@
 package api.routes.auth
 
 import akka.http.scaladsl.model.StatusCodes
-import akka.http.scaladsl.server.Directives
+import akka.http.scaladsl.server.{Directives, Route}
 import api.json.{Auth, AuthJsonSupport}
-import api.utils.Authentication
 import com.password4j.Password
-import database.models.Token
-import database.operations.{AuthQ, TokenQ}
-import java.util.UUID
-import scala.concurrent.Future
-import scala.util.{Failure, Success}
+import database.operations.AuthQ
+import java.util.{Date, UUID}
+import com.nimbusds.jose.crypto.RSASSASigner
+import com.nimbusds.jose.{JWSAlgorithm, JWSHeader}
+import com.nimbusds.jwt.{JWTClaimsSet, SignedJWT}
+import com.typesafe.config.{Config, ConfigFactory}
+import java.math.BigInteger
+import java.security.{KeyFactory, PrivateKey}
+import java.security.spec.RSAPrivateKeySpec
+import java.time.{Instant, ZoneId}
 
 
 class Post extends Directives with AuthJsonSupport {
 
-  val auth = new Authentication
-  val token = new TokenQ
-  val opr = new AuthQ
+  private val opr = new AuthQ
 
-  val route =
+  private val config: Config = ConfigFactory.load()
+
+  private val modulus = config.getString("MODULUS_KEY")
+  private val privateExponent = config.getString("PRIVATE_EXPONENT")
+
+  def generateAccessToken(userId: String, admin: Boolean): (String, String) = {
+    val jwsAlgorithm: JWSAlgorithm = JWSAlgorithm.RS256
+
+    val privateKeySpec = new RSAPrivateKeySpec(new BigInteger(modulus), new BigInteger(privateExponent))
+    val privateKeyFactory = KeyFactory.getInstance("RSA")
+    val privateKey: PrivateKey = privateKeyFactory.generatePrivate(privateKeySpec)
+
+    val jwsHeader = new JWSHeader.Builder(jwsAlgorithm).keyID(UUID.randomUUID().toString).build()
+    val expirationTime = Date.from(Instant.now().plusSeconds(7200))
+
+    val payload: JWTClaimsSet = new JWTClaimsSet.Builder()
+      .claim("id", userId)
+      .claim("admin", admin)
+      .expirationTime(expirationTime)
+      .build()
+
+    val signedJWT = new SignedJWT(jwsHeader, payload)
+    signedJWT.sign(new RSASSASigner(privateKey))
+
+    (signedJWT.serialize(),expirationTime.toInstant.atZone(ZoneId.systemDefault).toLocalDateTime.toString)
+  }
+
+  val route: Route =
     path("auth") {
       post {
         entity(as[Auth]) { auth =>
           val user = opr.getUserByUsername(auth.username)
           if (Password.check(auth.password, user.password).withArgon2()) {
-            val tokenString = UUID.randomUUID().toString
-            val newToken = Token(id=null,revision=null,token=tokenString, user=auth.username)
-            val insertToken: Future[Unit] = token.addNewToken(newToken)
-            onComplete(insertToken) {
-              case Success(_) => complete(StatusCodes.OK, Map("token" -> tokenString, "expire" -> newToken.expire))
-              case Failure(exception) =>
-                println(exception)
-                complete(StatusCodes.InternalServerError, exception.toString)
-            }
+            val infoToken = generateAccessToken(user.id, user.admin)
+            complete(StatusCodes.OK,Map("token" -> infoToken._1, "refresh_token" -> "", "expiration_time" -> infoToken._2))
           } else {
             complete(StatusCodes.Unauthorized)
           }
-
         }
       }
     }
-
 }
--- src/main/scala/api/utils/Auth.scala
+++ src/main/scala/api/utils/Auth.scala
@@ -1,13 +1,44 @@
 package api.utils
 
 import akka.http.scaladsl.server.directives.Credentials
+import com.nimbusds.jose.JWSVerifier
+import com.nimbusds.jose.crypto.RSASSAVerifier
+import com.nimbusds.jwt.{JWTClaimsSet, SignedJWT}
+import com.typesafe.config.{Config, ConfigFactory}
 import database.models.Token
 import database.operations.TokenQ
+import java.math.BigInteger
+import java.security.KeyFactory
+import java.security.interfaces.RSAPublicKey
+import java.security.spec.RSAPublicKeySpec
 import java.time.format.DateTimeFormatter
 import scala.concurrent.duration.DurationInt
 import scala.concurrent.{Await, Future}
 
 class Authentication {
+
+  private val config: Config = ConfigFactory.load()
+
+  val modulus = config.getString("MODULUS_KEY")
+  val publicExponent = config.getString("PUBLIC_EXPONENT")
+
+  val publicKeySpec = new RSAPublicKeySpec(new BigInteger(modulus), new BigInteger(publicExponent))
+  val publicKeyFactory = KeyFactory.getInstance("RSA")
+  val publicKey: RSAPublicKey = publicKeyFactory.generatePublic(publicKeySpec).asInstanceOf[RSAPublicKey]
+
+  def validateToken(token: String): Boolean = {
+    val signedJWT = SignedJWT.parse(token)
+
+    val verifier: JWSVerifier = new RSASSAVerifier(publicKey)
+
+    if (signedJWT.verify(verifier)) {
+      val claims: JWTClaimsSet = signedJWT.getJWTClaimsSet
+      val expirationTime: Long = claims.getExpirationTime.getTime
+      val currentTime: Long = System.currentTimeMillis()
+      if (currentTime < expirationTime) true else false
+    } else false
+
+  }
 
   def verifyToken(token:String): Boolean = {
 
@@ -25,7 +56,7 @@ class Authentication {
   def myUserPassAuthenticator(credentials: Credentials): Option[Boolean] = {
 
     credentials match {
-      case p@Credentials.Provided(_) if p.provideVerify(verifier=verifyToken) => Some(true)
+      case p@Credentials.Provided(_) if p.provideVerify(verifier=validateToken) => Some(true)
       case _ => None
     }
   }
