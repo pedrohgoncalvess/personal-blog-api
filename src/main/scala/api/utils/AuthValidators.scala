f3f713bcf21150212204a23d9f6b2e7c8432ab69
unknown <pedro_gonsalves@hotmail.com>
2024-02-27 12:41:38 -0300

    fix: change name auth to authvalidators and add generateaccesstoken function

--- src/main/scala/api/utils/AuthValidators.scala
+++ src/main/scala/api/utils/AuthValidators.scala
@@ -5,17 +5,19 @@ import com.nimbusds.jose.JWSVerifier
 import com.nimbusds.jose.crypto.RSASSAVerifier
 import com.nimbusds.jwt.{JWTClaimsSet, SignedJWT}
 import com.typesafe.config.{Config, ConfigFactory}
-import database.models.Token
-import database.operations.TokenQ
+
 import java.math.BigInteger
 import java.security.KeyFactory
 import java.security.interfaces.RSAPublicKey
 import java.security.spec.RSAPublicKeySpec
-import java.time.format.DateTimeFormatter
-import scala.concurrent.duration.DurationInt
-import scala.concurrent.{Await, Future}
+import java.security.PrivateKey
+import java.security.spec.RSAPrivateKeySpec
+import java.time.{Instant, ZoneId}
+import com.nimbusds.jose.crypto.RSASSASigner
+import com.nimbusds.jose.{JWSAlgorithm, JWSHeader}
+import java.util.{Date, UUID}
 
-class Authentication {
+class AuthValidators {
 
   private val config: Config = ConfigFactory.load()
 
@@ -40,24 +42,39 @@ class Authentication {
 
   }
 
-  def verifyToken(token:String): Boolean = {
-
-    val tkn = new TokenQ
-    val getToken:Future[Token] = tkn.getTokenByToken(token)
-    try {
-      val result: Token = Await.result(getToken, 5.seconds)
-      result.token == token && java.time.LocalDateTime.parse(result.expire,DateTimeFormatter.ISO_OFFSET_DATE_TIME).isBefore(java.time.LocalDateTime.now)
-
-    } catch {
-      case _: Throwable => false
-    }
-  }
-
   def myUserPassAuthenticator(credentials: Credentials): Option[Boolean] = {
 
     credentials match {
       case p@Credentials.Provided(_) if p.provideVerify(verifier=validateToken) => Some(true)
       case _ => None
     }
+  }
+
+  def generateAccessToken(userId: String, admin: Boolean): (String, String) = {
+
+    val config: Config = ConfigFactory.load()
+
+    val modulus = config.getString("MODULUS_KEY")
+    val privateExponent = config.getString("PRIVATE_EXPONENT")
+
+    val jwsAlgorithm: JWSAlgorithm = JWSAlgorithm.RS256
+
+    val privateKeySpec = new RSAPrivateKeySpec(new BigInteger(modulus), new BigInteger(privateExponent))
+    val privateKeyFactory = KeyFactory.getInstance("RSA")
+    val privateKey: PrivateKey = privateKeyFactory.generatePrivate(privateKeySpec)
+
+    val jwsHeader = new JWSHeader.Builder(jwsAlgorithm).keyID(UUID.randomUUID().toString).build()
+    val expirationTime = Date.from(Instant.now().plusSeconds(7200))
+
+    val payload: JWTClaimsSet = new JWTClaimsSet.Builder()
+      .claim("id", userId)
+      .claim("admin", admin)
+      .expirationTime(expirationTime)
+      .build()
+
+    val signedJWT = new SignedJWT(jwsHeader, payload)
+    signedJWT.sign(new RSASSASigner(privateKey))
+
+    (signedJWT.serialize(),expirationTime.toInstant.atZone(ZoneId.systemDefault).toLocalDateTime.toString)
   }
 }
