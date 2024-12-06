348a9dbd6c34be3c6a7bf4ba7fbb6804738f33d8
Pedro <pedro_gonsalves@hotmail.com>
2024-12-06 12:13:37 -0300

    add: connection with postgresql

--- src/main/scala/database/Connection.scala
+++ src/main/scala/database/Connection.scala
@@ -1,25 +1,7 @@
 package database
 
-import com.typesafe.config.{Config, ConfigFactory}
-import org.ektorp.CouchDbConnector
-import org.ektorp.http.StdHttpClient
-import org.ektorp.impl.StdCouchDbInstance
+import slick.jdbc.PostgresProfile.api._
 
-
-class CouchConnection {
-
-  private val config: Config = ConfigFactory.load()
-  private val dbName = config.getString("DB_NAME")
-  private val dbUrl = s"http://${config.getString("DB_HOST")}:${config.getInt("DB_PORT")}"
-
-  def couchInstance[T](operation: CouchDbConnector => T): T = {
-    val httpClient = new StdHttpClient.Builder().url(dbUrl).username(config.getString("DB_USERNAME")).password(config.getString("DB_PASSWORD")).build()
-    val dbInstance = new StdCouchDbInstance(httpClient)
-
-    val db = dbInstance.createConnector(dbName, false)
-
-    try {
-      operation(db)
-    }
-  }
+object Connection {
+  val db = Database.forConfig("postgres")
 }
