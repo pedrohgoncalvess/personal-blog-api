84dc36baccfe70ed526cb4251d442e2a5d564641
Pedro H. <pedro_gonsalves@hotmail.com>
2025-01-16 17:36:35 -0300

    add: path prefix for all endpoints and add directives herence

--- src/main/scala/api/routes/admin/article/Router.scala
+++ src/main/scala/api/routes/admin/article/Router.scala
@@ -1,22 +1,25 @@
 package api.routes.admin.article
 
+
 import akka.http.scaladsl.server.Directives.concat
-import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.server.{Directives, Route}
+
 import api.cors.GenericCors
-import api.routes.auth.refresh.Post
 
 
-class Router extends GenericCors:
+class Router extends GenericCors with Directives:
   private val deletePath = Delete()
   private val getPath = Get()
   private val patchPath = Patch()
   private val postPath = Post()
   
   val router: Route = withCors {
-    concat(
-      deletePath.route,
-      getPath.route,
-      patchPath.route,
-      postPath.route
-    )
+    pathPrefix("admin") {
+      concat(
+        deletePath.route,
+        getPath.route,
+        patchPath.route,
+        postPath.route
+      )
+    }
   }
