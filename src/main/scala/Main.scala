12d56d89e86d11dc60ebf8cd00588b3d55359e9f
Pedro H. <pedro_gonsalves@hotmail.com>
2025-01-18 01:46:12 -0300

    add: includes migration

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,11 +4,12 @@ import scala.io.StdIn
 import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.Behaviors
 import akka.http.scaladsl.Http
-
 import akka.http.scaladsl.server.{Directives, Route}
+
 import api.routes.admin.article.Router as AdminRouter
 import api.routes.auth.Router as AuthRouter
 import api.routes.article.Router as PublicRouter
+import database.migration.Main.flyway
 
 
 class Routes extends Directives:
@@ -22,12 +23,13 @@ class Routes extends Directives:
 
 object HttpServer extends App:
 
-    implicit val system: ActorSystem[Nothing] = ActorSystem(Behaviors.empty, "CthaehBlogApi")
+    println("Executing migrations...")
+    flyway.migrate()
 
+    implicit val system: ActorSystem[Nothing] = ActorSystem(Behaviors.empty, "CthaehBlogApi")
     implicit val executionContext: ExecutionContextExecutor = system.executionContext
 
     private val service = new Routes
-
     private val port = 9001
 
     private val bindingFuture = Http().newServerAt("0.0.0.0", port).bind(service.route)
